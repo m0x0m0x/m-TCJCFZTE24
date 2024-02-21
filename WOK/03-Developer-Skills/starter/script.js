f33309dcdbc96a42cabe6f889ccc2ceb2b424647
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2024-02-21 08:58:02 +0000

    .

--- WOK/03-Developer-Skills/starter/script.js
+++ WOK/03-Developer-Skills/starter/script.js
@@ -1,3 +1,16 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+console.log(
+  ` %c
+Testing some basics 
+`,
+  'color: red'
+);
+
+const manVal = 232;
+//Ternary Operator testing
+manVal === 23 ? console.log('yes') : console.log('no');
+
+const calcAge = birthYear => 2037 - birthYear;
+console.log(calcAge(1991));
