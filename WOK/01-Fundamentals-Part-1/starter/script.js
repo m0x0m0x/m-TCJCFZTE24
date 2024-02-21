1dbe042e5ab8fa9994bb065ef9fb15e5062b2e07
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2024-02-21 08:42:26 +0000

    .

--- WOK/01-Fundamentals-Part-1/starter/script.js
+++ WOK/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,13 @@
 let js = "amazing";
-if (js === "amazing") alert("Sucki");
+// if (js === "amazing") alert("Sucki");
 
 // Runing some math
-console.log(Math.log(2));
+// console.log(Math.log(2));
+
+// Variable assignment
+console.log("Booty");
+console.log(23);
+
+// Variable assignment
+let firstName = "Miss";
+console.log(firstName);
